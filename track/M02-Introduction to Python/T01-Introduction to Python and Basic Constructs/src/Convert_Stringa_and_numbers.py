704e0551742dd7c6e83f001fb811598fc79f4ae1
Lahari0314 <laharisingamsetty2005@gmail.com>
2026-08-16 10:25:08 +0530

    M02-T01-type converstion and reassignment

---
+++ track/M02-Introduction to Python/T01-Introduction to Python and Basic Constructs/src/Convert_Stringa_and_numbers.py
@@ -0,0 +1,11 @@
+age_text = "25"
+course_fee_text="499.50"
+attempts=3
+
+age=int(age_text)
+course_fee=float(course_fee_text)
+attempts=str(attempts)
+
+print(age,type(age))
+print(course_fee,type(course_fee))
+print(attempts,type(attempts))
