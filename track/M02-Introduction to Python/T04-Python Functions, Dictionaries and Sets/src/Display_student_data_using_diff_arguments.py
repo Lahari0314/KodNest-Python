dd58ffb609fa4b6c472d1cdd07814aff13cdaad1
Lahari0314 <laharisingamsetty2005@gmail.com>
2026-08-24 11:26:06 +0530

    Displaying student details using the arguments

---
+++ track/M02-Introduction to Python/T04-Python Functions, Dictionaries and Sets/src/Display_student_data_using_diff_arguments.py
@@ -0,0 +1,6 @@
+def student_detail(name,course,level):
+    print(f"{name} | {course} | {level}")
+
+student_detail("Arav","Python","Beginner")
+student_detail("Meera",course="Java",level="Advanced")
+student_detail(level="Intermediate",name="Arun",course="Python")
